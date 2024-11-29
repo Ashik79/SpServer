19fbc6130b82e8ff284b663cbb620570c4b157fe
unknown <asifdinar7@gmail.com>
2024-11-29 15:37:01 +0600

    limit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 const XLSX = require('xlsx');
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Adjust the limit as needed
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 
 app.listen(port, () => {
